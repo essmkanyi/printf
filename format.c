4c37c2a996993805b5ffa4c81d2f82603d018198
Darryl Mbae <darrylmbae01@gmail.com>
2023-04-25 16:07:13 -0700

    Fixing error

--- format.c
+++ format.c
@@ -2,50 +2,11 @@
 #include <stdarg.h>
 
 /**
- * print_char - prints a single character
- * @arg: character argument
- * @count: pointer to character count
- */
-void print_char(va_list arg, int *count)
-{
-	char c = va_arg(arg, int);
-
-	putchar(c);
-	(*count)++;
-}
-
-/**
- * print_str - prints a string
- * @arg: string argument
- * @count: pointer to character count
- */
-void print_str(va_list arg, int *count)
-{
-	const char *s = va_arg(arg, const char *);
-
-	while (*s)
-	{
-		putchar(*s);
-		s++;
-		(*count)++;
-	}
-}
-
-/**
- * print_percent - prints a percent character
- * @count: pointer to character count
- */
-void print_percent(int *count)
-{
-	putchar('%');
-	(*count)++;
-}
-
-/**
- * _printf - custom printf implementation
- * @format: format string
+ * _printf - Prints formatted output to stdout.
  *
- * Return: number of characters printed (excluding null byte)
+ * @format: Format string.
+ *
+ * Return: Number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
@@ -53,25 +14,32 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_start(args, format);
-	while (*format)
+
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
+			if (*format == 'c')
 			{
-				case 'c':
-					print_char(args, &count);
-					break;
-				case 's':
-					print_str(args, &count);
-					break;
-				case '%':
-					print_percent(&count);
-					break;
-				default:
-					/* unsupported conversion specifier */
-					break;
+				int c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
 			}
 		}
 		else
@@ -79,9 +47,12 @@ int _printf(const char *format, ...)
 			putchar(*format);
 			count++;
 		}
+
 		format++;
 	}
+
 	va_end(args);
+
 	return (count);
 }
 
